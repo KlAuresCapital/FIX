e9c2d30c01c6ff603ec0fadd6c14a257817b2f87
Lin Kai <kl@aurescapital.com>
2023-02-02 22:19:23 +0800

    Added new xml file

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -2,25 +2,63 @@ package Client;
 import quickfix.*;
 import quickfix.field.*;
 import quickfix.fix44.NewOrderSingle;
+import quickfix.fix44.Logon;
+import quickfix.field.Username;
 
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+
 public class Client {
-    public static void main(String[] args) throws FileNotFoundException, ConfigError, InterruptedException {
-        String fileName = "src/clientTest.cfg";
-        ClientApp application = new ClientApp();
-        SessionSettings settings = new SessionSettings(new FileInputStream(fileName));
-        MessageStoreFactory storeFactory = new FileStoreFactory(settings);
-        LogFactory logFactory = new FileLogFactory(settings);
-        MessageFactory messageFactory = new DefaultMessageFactory();
-        Initiator initiator = new SocketInitiator(application, storeFactory, settings, logFactory, messageFactory);
-        initiator.start();
-        while (ClientApp.sessionID == null) {
-            Thread.sleep(1000);
+    public static void main(String[] args) {
+        SocketInitiator socketInitiator = null;
+        try {
+            SessionSettings initiatorSettings = new SessionSettings(
+                    "src/currenExConnect.cfg");
+            Application initiatorApplication = new ClientApp();
+            FileStoreFactory fileStoreFactory = new FileStoreFactory(
+                    initiatorSettings);
+            FileLogFactory fileLogFactory = new FileLogFactory(
+                    initiatorSettings);
+            MessageFactory messageFactory = new DefaultMessageFactory();
+            socketInitiator = new SocketInitiator(initiatorApplication, fileStoreFactory, initiatorSettings, fileLogFactory, messageFactory);
+            socketInitiator.start();
+            SessionID sessionId =  (SessionID) socketInitiator.getSessions().get(0);
+            Session.lookupSession(sessionId).logon();
+
+            Logon logon = new Logon();
+
+            logon.set(new quickfix.field.HeartBtInt(30));
+            logon.set(new quickfix.field.ResetSeqNumFlag(true));
+            logon.set(new quickfix.field.EncryptMethod(0));
+
+
+            try {
+                Session.sendToTarget(logon, sessionId);
+            } catch (SessionNotFound sessionNotFound) {
+                sessionNotFound.printStackTrace();
+            }
+            for(int j = 0; j < 2; j ++){
+                try {
+                    Thread.sleep(10000);
+                    NewOrderSingle newOrderSingle = new NewOrderSingle(
+                            new ClOrdID("456"),
+                            new Side(Side.BUY),
+                            new TransactTime(),
+                            new OrdType(OrdType.MARKET)
+                    );
+                    System.out.println("####New Order Sent :" + newOrderSingle.toString());
+                    Session.sendToTarget(newOrderSingle, sessionId);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (SessionNotFound sessionNotFound) {
+                    sessionNotFound.printStackTrace();
+                }
+            }
+        }  catch (ConfigError configError) {
+            configError.printStackTrace();
         }
-
-
     }
 }
--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -7,23 +7,21 @@ import java.io.FileNotFoundException;
 import java.util.concurrent.CountDownLatch;
 
 public class Server {
-    public static void main(String[] args) throws FileNotFoundException, ConfigError, InterruptedException {
-        String fileName = "src/serverTest.cfg";
-        ServerApp application = new ServerApp();
-        SessionSettings settings = new SessionSettings(new FileInputStream(fileName));
-        MessageStoreFactory storeFactory = new FileStoreFactory(settings);
-        LogFactory logFactory = new FileLogFactory(settings);
-        MessageFactory messageFactory = new DefaultMessageFactory();
-        System.out.println(logFactory);
-        System.out.println("test");
-        Acceptor acceptor = new SocketAcceptor(application, storeFactory, settings, logFactory, messageFactory);
-        acceptor.start();
-        System.out.println("next");
-        CountDownLatch latch = new CountDownLatch(1);
-        latch.await();
-
-
-
-
+    public static void main(String[] args) {
+        SocketAcceptor socketAcceptor = null;
+        try {
+            SessionSettings executorSettings = new SessionSettings(
+                    "src/messageAcceptorTest.cfg");
+            Application application = new ServerApp();
+            FileStoreFactory fileStoreFactory = new FileStoreFactory(
+                    executorSettings);
+            MessageFactory messageFactory = new DefaultMessageFactory();
+            FileLogFactory fileLogFactory = new FileLogFactory(executorSettings);
+            socketAcceptor = new SocketAcceptor(application, fileStoreFactory,
+                    executorSettings, fileLogFactory, messageFactory);
+            socketAcceptor.start();
+        } catch (ConfigError configError) {
+            configError.printStackTrace();
+        }
     }
 }
